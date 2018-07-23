34fc714b2e413835269856f8ab0b13f5de0f99b2
LiuZichen <danteliuzichen@163.com>
2018-07-23 15:35:37 +0800

    球队数据信息

---
+++ frontend/views/world-cup-team/world-cup-team-info.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘子晨
+ * Date: 2018/7/23
+ * Time: 14:46
+ */
+
+$this->title='德塔贝斯-世界杯球队信息';
+?>
+<head>
+    <link href="css/world-cup-team/team_logo.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/world-cup-team/sitehead.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/world-cup-team/head_SenZTZaJwaIQ.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<div class="area clearfix">
+
+    <div class="col_l">
+<div class="team_statistics" ne-module="tabs" ne-state="showhide:true;events:click;">
+    <div class="team_statistics_navs clearfix">
+        <div class="team_statistics_nav current" ne-role="tab-nav">赛程</div>
+        <div class="team_statistics_nav" ne-role="tab-nav">球员统计</div>
+    </div>
+    <div class="team_statistics_cons">
+        <div class="team_statistics_con current" ne-role="tab-body" style="">
+            <ul class="team_statistics_schedule clearfix">
+
+                <li>
+                    <div class="round_type">
+                        小组赛
+                    </div>
+                    <div class="time">已结束</div>
+                    <div class="groups">
+                        <div class="home_group"><a href="http://sports.163.com/special/team4694/" target="_blank">俄罗斯</a></div>
+                        <div class="home_score">5</div>
+                        <div class="away_group"><a href="http://sports.163.com/special/team4834/" target="_blank">沙特阿拉伯</a></div>
+                        <div class="away_score">0</div>
+                    </div>
+                    <div class="links">
+                        <a>aaaa</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+    </div>
+</div>
